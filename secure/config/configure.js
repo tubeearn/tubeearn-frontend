7f13e199e9291f1f70aab493ab4ea6b5366d88cd
tubeearn <wwwsanjeevbarh95@gmail.com>
2025-08-09 18:38:22 +0530

    Update configure.js

--- secure/config/configure.js
+++ secure/config/configure.js
@@ -1,5 +1,4 @@
 const config = {
-  apiUrl: 'https://tubeearn-backend.onrender.com'  // यहाँ अपना रेंडर का प्राइमरी API URL डालो
+  apiUrl: 'https://tubeearn-backend.onrender.com'
 };
-
 export default config;
