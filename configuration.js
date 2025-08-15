2f83583af728804167ae3a0f1889878be323679b
tubeearn <wwwsanjeevbarh95@gmail.com>
2025-08-15 08:50:08 +0530

    configure.js

--- configuration.js
+++
@@ -1,2 +0,0 @@
-// Backend API Base URL (Global variable)
-const API_BASE = "https://tubeearn-backend.onrender.com";
---
+++ configure.js
@@ -0,0 +1,5 @@
+// Backend और अन्य global settings
+const CONFIG = {
+  API_BASE_URL: "https://tubeearn-backend.onrender.com"
+};
+export default CONFIG;
