1e321a06cc038ef07260e08c14456c5231ae62a4
tubeearn <wwwsanjeevbarh95@gmail.com>
2025-08-15 10:21:54 +0530

    Update url.js

--- url/url.js
+++ url/url.js
@@ -1,5 +1,5 @@
-// Backend और अन्य global settings
+// frontend/url/url.js
 const CONFIG = {
-  API_BASE_URL: "https://tubeearn-backend.onrender.com"
+  API_BASE_URL: "https://tubeearn-backend.onrender.com" // आपका backend URL यहाँ डालें
 };
 export default CONFIG;
